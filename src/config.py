4e2ed6f4983fb19ddea7464bccb866990d817e31
IAmSuyogJadhav <suyog.jadhav1@gmail.com>
2019-05-30 20:11:36 +0530

    Modified the config for own use

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ class Config:
     max_size = 1000 # image resize
     caffe_pretrain = True
     caffe_pretrain_path = './data/pretrained_model/vgg16_caffe.pth'
-    model_save_path = './checkpoints'  # MODIFIED
+    model_save_path = './checkpoints'
     # sigma for l1_smooth_loss
     rpn_sigma = 3.
     test_output_path = './output'
@@ -22,7 +22,7 @@ class Config:
 
     pretrained_model = 'vgg16'
 
-    epoch = 15
+    epoch = 10
 
     use_adam = False # Use Adam optimizer
     use_chainer = False # try match everything as chainer
